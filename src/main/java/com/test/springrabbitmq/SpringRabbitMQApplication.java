cde216e7782514d21a225afe11ffb774a07a6b83
Edwardwong0826 <edwardwong_94@outlook.com>
2021-06-05 23:35:33 +0800

    Spring RabbitMQ start info

--- src/main/java/com/test/springrabbitmq/SpringRabbitMQApplication.java
+++ src/main/java/com/test/springrabbitmq/SpringRabbitMQApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringRabbitMQApplication {
-
+	// run this spring boot application, go to test folder TestRabbitMQ each type to send the message only can see
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRabbitMQApplication.class, args);
 	}
