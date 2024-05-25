38893282d9088c0abcd98dbe9024a16c8a08358f
Edwardwong0826 <edwardwong_94@outlook.com>
2024-05-25 20:36:37 +0800

    Idempotency
    Add changes for enabled publisher send message will generate unique UUID for idempotency purpose

--- src/main/java/com/test/springrabbitmq/SpringRabbitMQApplication.java
+++ src/main/java/com/test/springrabbitmq/SpringRabbitMQApplication.java
@@ -1,7 +1,10 @@
 package com.test.springrabbitmq;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRabbitMQApplication {
@@ -10,6 +13,15 @@ public class SpringRabbitMQApplication {
 	// run this spring boot application, go to test folder RabbitMQ each class and type to send the message only can see
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRabbitMQApplication.class, args);
+	}
+
+	@Bean
+	public MessageConverter jacksonMessageConvertor(){
+
+		Jackson2JsonMessageConverter jjmc = new Jackson2JsonMessageConverter();
+		jjmc.setCreateMessageIds(true);
+		return jjmc;
+
 	}
 
 }
